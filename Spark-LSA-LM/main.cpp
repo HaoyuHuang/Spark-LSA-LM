d587f5087ea7b197709e04d47e1d39ac92064e60
AronHuang <aronexplorer@gmail.com>
2014-04-16 22:31:19 -0700

    Initial Commit

---
+++ Spark-LSA-LM/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Spark-LSA-LM
+//
+//  Created by Haoyu Huang on 4/16/14.
+//  Copyright (c) 2014 HaoyuHuang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
