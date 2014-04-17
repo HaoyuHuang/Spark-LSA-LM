6296a3b754f2b27a1629201772fe79e07fe595fa
AronHuang <aronexplorer@gmail.com>
2014-04-16 22:37:29 -0700

    init commit

---
+++ Spark-LSA-LM/common.h
@@ -0,0 +1,5 @@
+#include<cstring>
+#include<cstdlib>
+
+typedef unsigned int word_cnt;
+
---
+++ Spark-LSA-LM/word.h
@@ -0,0 +1,9 @@
+#include<vector>
+#include "utils/common.h"
+#include<cstring>
+
+typedef struct word
+{
+	vector<word_cnt> cnt;
+	string word;
+}word_t;
