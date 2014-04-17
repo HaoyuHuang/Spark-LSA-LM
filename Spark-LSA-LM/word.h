63f31c4d337b3c6d46054fe926f1d9c04e6cf2fe
1guangnian <scorpio.uestc@gmail.com>
2014-04-16 23:05:27 -0700

    add cosine.h cosine.cpp

--- Spark-LSA-LM/word.h
+++ Spark-LSA-LM/word.h
@@ -1,9 +1,20 @@
-#include<vector>
-#include "utils/common.h"
-#include<cstring>
+#include <vector>
+#include "common.h"
+#include <cstring>
 
+typedef struct entry
+{
+	int cnt, doc_id;
+}matrix_entry_t;
 typedef struct word
 {
-	vector<word_cnt> cnt;
+	vector<matrix_entry_t> list;
 	string word;
+	double lsa_conf;
+	/* prob of word given document c, p(w|c)*/
+	vector<double> p_wc;
+	/* likelihood of document j given that term i occurs in j
+	 * count of term i in document j / count of term i in whole corpus
+	 * */
+	vector<double> p_ji;
 }word_t;
