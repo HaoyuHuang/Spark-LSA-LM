35704bfed2a7a73bc3cc544ff6d0764d9b812848
AronHuang <aronexplorer@gmail.com>
2014-04-19 12:52:53 -0700

    ngram commit

---
+++ Spark-LSA-LM/lsa_ngram.cpp
@@ -0,0 +1,78 @@
+#include "lsa_word_vector.h"
+#include "fst_common.h"
+#include "utils.h"
+#include "lsa_common.h"
+#include "lsa_math.h"
+using namespace std;
+int lsa_ngram_load(const char *symbfn, vector<unordered_map<std::string, double> > &ngram)
+{
+    std::string line;
+    std::ifstream fin (symbfn);
+    int max_order = 0;
+    ngram.clear();
+    if (fin.is_open()) {
+        while (getline(fin, line)){
+            if (line == "\\Data\\") {
+                break;
+            }
+        }
+        while (getline(fin, line) && line.length() > 0) {
+            max_order++;
+            /*
+            std::istringstream in(line);
+            std::string str;
+            
+            in >> str;
+            in >> str;
+            int i = 0;
+            while (i < str.length() && str[i] != '=')
+                i++;
+            i++;
+            uword num = 0;
+            */
+        }
+        for (int i = 1; i  <= max_order; i++) {
+            getline(fin, line); // \i-grams:
+            unordered_map<std::string, double> mp;
+            while (getline(fin, line) && line.length() > 0) {
+                std::istringstream in(line);
+                std::string str;
+                std::string input_ngram = "";
+                bool has_backoff = false;
+                double prob, backoff;
+                in >> prob;
+                for (int j = 0; j < i; j++) {
+                    in >> str;
+                    if (j > 0) {
+                        input_ngram += "@@";
+                    }
+                    input_ngram += str;
+                }
+                mp.emplace(input_ngram, prob);
+                if (in >> backoff)
+                    has_backoff = true;
+            }
+            ngram.push_back(mp);
+        }
+        getline(fin, line); // end
+    }
+    else {
+        spark_debug("open file %s failed\n", symbfn);
+        return -1;
+    }
+    return 0;
+}
+std::string lsa_ngram_combine(vector<std::string > &mygram) {
+    std::string res = "";
+    for (int i = 0; i < mygram.size(); i++) {
+        if (i > 0)
+            res += "@@";
+        res += mygram[i];
+    }
+    return res;
+}
+double lsa_ngram_prob(std::string &mygram, int N, vector<unordered_map<std::string, double> > &ngram)
+{
+    std::unordered_map<std::string,double>::const_iterator got = ngram[N - 1].find (mygram);
+    return got->second;
+}
--- Spark-LSA-LM/lsa_ngram.h
+++ Spark-LSA-LM/lsa_ngram.h
@@ -17,8 +17,11 @@ typedef struct lsa_ngram_arpa {
 } ngram_t;
 
 // load arpa file into ngram
-void lsa_ngram_load(const char *symbfn, unordered_map<std::string, double> &ngram);
+void lsa_ngram_load(const char *symbfn, vector<unordered_map<std::string, double> > &ngram);
 // write ngram into arpa file
-void lsa_ngram_arpa(const char *symbfn, unordered_map<std::string, double> &ngram);
-
+void lsa_ngram_arpa(const char *symbfn, vector<unordered_map<std::string, double> > &ngram);
+//make n words into one
+std::string lsa_ngram_combine(vector<std::string > &mygram);
+// calculate the log10(prob) of ngram in mygram, the order is N
+double lsa_ngram_prob(std::string &mygram, int N, vector<unordered_map<std::string, double> > &ngram);
 #endif
